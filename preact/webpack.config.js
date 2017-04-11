aa20adf81c01a74e57d3b439eb3a1d4fd1b19f15
Pascal Widdershoven <pascal@kabisa.nl>
2017-04-11 08:17:22 +0200

    Fix preact build
    Preact-router is distributed as an es6 module, which uglifyjs can't
    handle.

--- preact/webpack.config.js
+++ preact/webpack.config.js
@@ -7,10 +7,29 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'app.js',
   },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include: [
+          path.resolve(__dirname, "src/"),
+          path.resolve(__dirname, "node_modules/preact-router")
+        ],
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env', 'babili']
+          }
+        }
+      }
+    ]
+  },
   node: {
     process: false
   },
   plugins: [
+    new webpack.LoaderOptionsPlugin({minimize: true, debug: false}),
+    new webpack.DefinePlugin({'process.env.NODE_ENV': JSON.stringify('production')}),
     new webpack.optimize.UglifyJsPlugin({minimize: true})
   ],
   performance: {
